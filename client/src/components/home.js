e29f16a579aeb279828088dbaa4ff56adce07881
ralucaantal <antalvaidaraluca20@stud.ase.ro>
2023-01-12 15:29:22 +0200

    am inceput sa fac pagina profesorului

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,24 +6,29 @@ export default function Home() {
   return (
     <div className="main">
       <div className="home">
-        <h1>Bine ati venit!</h1>
-        <div className="login-btn">
-          <button
-            className="btn1"
-            onClick={() => {
-              window.location.href = "/login";
-            }}
-          >
-            LOGHEAZA-TE
-          </button>
-        </div>
-        <h2>Nu ai un cont?</h2>
-        <div className="login-btn">
-          <button className="btn1"
-          onClick={()=>{
-            window.location.href="/register"
-          }}
-          >INREGISTREAZA-TE</button>
+        <div className="cover" style={{width: "25em", height:"15em"}}>
+          <h1>Bine ai venit!</h1>
+          <div className="login-btn">
+            <button
+              className="btn1"
+              onClick={() => {
+                window.location.href = "/login";
+              }}
+            >
+              LOGHEAZA-TE
+            </button>
+          </div>
+          <h2>Nu ai un cont?</h2>
+          <div className="login-btn">
+            <button
+              className="btn1"
+              onClick={() => {
+                window.location.href = "/register";
+              }}
+            >
+              INREGISTREAZA-TE
+            </button>
+          </div>
         </div>
       </div>
     </div>
