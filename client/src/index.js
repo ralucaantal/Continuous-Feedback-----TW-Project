e64dd1dfb5906d84c650c835bfc91086fcde6be1
ralucaantal <antalvaidaraluca20@stud.ase.ro>
2023-08-22 20:52:17 +0300

    versiune actuala

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-let acasa = "http://192.168.1.130"
-let hotspot="http://172.20.10.3"
+let acasa = "http://192.168.1.11";
+let hotspot = "http://172.20.10.3";
+let facultate = "http://10.30.5.148";
 
 let IPv4;
 export default IPv4 = acasa;
 //export default IPv4 = hotspot;
+//export default IPv4 = facultate;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
